383d47aaa8019f00fd2362fc9df35998e2557d93
Janet Matsen <janetmatsen@gmail.com>
2016-12-02 12:25:14 -0800

    sketch of u, v prep + class to keep arrays in; need to fill in counting & plotting methods

---
+++ code/CCA.py
@@ -0,0 +1,29 @@
+class CCA:
+    def __init__(self, x, z, u, v, val_x, val_z):
+        self.x = x
+        self.z = z
+        self.u = u
+        self.v = v
+        self.val_x
+        self.val_z
+
+    def num_nonzero_components(self):
+        # count # of nonzero components in u and v
+        pass
+
+    def project_x(self):
+        return self.x.dot(u)
+
+    def project_val_x(self):
+        return self.val_x.dot(self.u)
+
+    def project_z(self):
+        return self.z.dot(self.v)
+
+    def project_val_z(self):
+        return self.val_z.dot(self.v)
+
+    def plot_projections(self):
+        # scatter plot of x.dot(u) vs z.dot(v)
+        # one color for train, one color for validation
+        pass
